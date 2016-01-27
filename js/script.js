db210ca8481aa78192b2dd3365a2d109609be65c
Sergey Yeremenko <syeremenko@live.com>
2016-01-26 22:54:47 -0500

    setup new design

--- js/script.js
+++ js/script.js
@@ -1,32 +1,3 @@
 $(document).ready(function () {
 
-	var $window = $(window),
-		$portLink = $('#portfolio-list li a');
-
-	var adjustHeight = function () {
-		var $winW   = $(window).width(),
-			$winH   = $(window).height(),
-			$hero	= $('#hero');
-			
-		$hero.css('height' , $winH);
-
-		// console.log($winH,$winW);
-	};
-
-	// $('header a , #cta-btn').on('click' , function (evt) {
-	//  	evt.preventDefault();
-	 	
-	//  	var url = $(this).attr('href');
-	//  	$('#main-wrap').load(url + ' ' + '#main');
-	//  });
-
-	adjustHeight();
-
-	$window.on('resize' , adjustHeight);
-
-	$portLink.hover( function(){
-		$(this).find('.excerpt').fadeToggle('fast');
-		// console.log($(this));
-	});
-
 });
