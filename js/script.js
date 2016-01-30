9325dd1d63b2713a0f6f26ed6852283c56fd145f
Sergey Yeremenko <syeremenko@live.com>
2016-01-30 01:45:25 -0500

    Add lighbox plugin

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
+  var imgOverlay = function () {
+    $('.portfolio-item-link').hover(function () {
+      $('<div class="img-overlay"></div>')
+        .prependTo(this);
+    } , function () {
+      $('.img-overlay').remove();
+    });
+  };
 
+  var initialize = function () {
+    imgOverlay();
+  };
+
+  initialize();
 });
