6f8e03a12e152ae99fd44795b5b51282a0221b3c
Sergey Yeremenko <syeremenko@live.com>
2016-02-14 02:51:19 -0500

    Functionality for single page navigation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 $(document).ready(function () {
+  var headerLinks = function () {
+    var $headerLinks = $('#header-nav a'),
+        selectedLink,
+        selectedWrap;
+
+    $headerLinks.on('click' , function (e) {
+      e.preventDefault();
+
+      selectedLink = $(this);
+      selectedWrap = '#' + selectedLink.data('show-section');
+
+      if ( $(selectedWrap).is(':visible') ) {
+        return;
+      } else {
+        $headerLinks.removeClass('active');
+        selectedLink.addClass('active');
+
+        if ( selectedWrap == '#portfolio-wrap') {
+          initMixItUp();
+        }
+
+        $('.hide-wrapper').fadeOut(200);
+        $(selectedWrap).fadeIn(700);
+      }
+    });
+  };
   var imgOverlay = function () {
     $('.portfolio-item-link').hover(function () {
       $('<div class="img-overlay"></div>')
@@ -7,17 +33,18 @@ $(document).ready(function () {
       $('.img-overlay').remove();
     });
   };
-
-  var initialize = function () {
-    $('#portfolio').mixItUp(
-      {
+  var initMixItUp = function () {
+    $('#portfolio').mixItUp({
         animation: {
           duration: 400,
           effects: 'fade',
           easing: 'cubic-bezier(0.39, 0.575, 0.565, 1)'
         }
-      }
-    );
+      });
+  };
+  var initialize = function () {
+    $('#landing-wrap').fadeIn(900);
+    headerLinks();
     imgOverlay();
   };
 
